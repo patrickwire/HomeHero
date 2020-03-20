cab3438ad6c8d96036095794d4648aea5f584c01
Patrick Müller <patrick@smokeless.world>
2020-03-20 21:34:39 +0100

    added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { firebaseConfig } from './firebase.config';
+firebaseConfig
 function App() {
   return (
     <div className="App">
---
+++ src/firebase.ts
@@ -0,0 +1,5 @@
+import firebase from "firebase";
+import { firebaseConfig } from "./firebase.config";
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
