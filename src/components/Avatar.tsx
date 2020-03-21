25d99a6e4a9acb5fcfab7a8e86d8d6685a2f1823
Patrick Müller <patrick@smokeless.world>
2020-03-21 12:29:53 +0100

    defautl avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState, useContext } from "react";
 import "./Avatar.css";
+import asset from "../assets/avatar_preview.svg"
 interface Props{
     url:string;
 }
 
 export const Avatar=(props:Props)=> {  
 return(
-    <div  className="avatarFrame"><img src={props.url}/></div>
+    <div  className="avatarFrame"><img src={props.url||asset}/></div>
    );
   }
