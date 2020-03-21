4643658f96e945b45a9c224ac491d0f0f93f40e9
Patrick Müller <patrick@smokeless.world>
2020-03-21 12:02:35 +0100

    add task function

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -3,12 +3,20 @@ import { firestore } from "firebase";
 import { AuthContext } from "../App";
 import { UserData } from "../interfaces/userData";
 import { Avatar } from "../components/Avatar";
+import { Button } from "@material-ui/core";
+import moment  from "moment";
 
 
 export const Home = () => {
   const [username, setUsername] = useState("");
   const [avatar, setAvatar] = useState("");
+  const [actions, setAction] = useState([]);
   const userAuth = useContext(AuthContext);
+  const addTask = ()=>{
+    firestore()
+        .collection("users")
+        .doc(userAuth.uid).update({actions:firestore.FieldValue.arrayUnion( {type:"test",title:"do good stuff",points:500,date:moment().toISOString()} )})
+  }
   useEffect(() => {
     if (userAuth.uid) {
       firestore()
@@ -30,6 +38,7 @@ export const Home = () => {
       {username !== "" && "hello " + username}
       <br />
       <Avatar url={avatar} />
+      <Button onClick={()=>{addTask()}}>Add tasks</Button>
     </div>
   );
 };
