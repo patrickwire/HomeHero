867d517b332e31bd0b66acb6ca891094863c4a34
lucas <lucas@unetiq.com>
2020-03-21 12:47:20 +0100

    finished User Point View

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -7,6 +7,8 @@ import { Avatar } from "../components/Avatar";
 import banner_orange from "../assets/banner_orange.svg"
 import banner_blue from "../assets/banner_blue.svg"
 import banner_red from "../assets/banner_red.svg"
+import icon_star_white from "../assets/icon_star_white.svg"
+import icon_star_purple from "../assets/icon_star_purple.svg"
 
 
 export const Home = () => {
@@ -22,7 +24,7 @@ export const Home = () => {
         .then(snapshot => {
           if (snapshot && snapshot.exists) {
             const data = snapshot.data() as UserData;
-            setUsername(data.username);
+            setUsername(data.username.toUpperCase());
             setAvatar(data.avatar);
           }
         });
@@ -31,23 +33,45 @@ export const Home = () => {
   return (
     <div>
         <div className="ProfileContainer">
+          <div className="ProfilePic">
             <Avatar url={avatar} />
-            {username !== "" && "hello " + username}
+            <div className="User">
+              <div className="Username">{username !== "" && username}</div>
+              <div className="Userpoints">
+                <b>9.999</b>
+                <img style={{height: '32px', paddingLeft: '7px'}} src={icon_star_purple}></img>
+              </div>
+              <div className="Descriptor">STERNE</div>
+            </div>
+          </div>
+            
         </div>
         <div className="SVGContainer">
-            <div className="Behavior">
-                 <img src={banner_orange}></img>
-                 <b className="Points">9.999</b><br />
+            <div className="Social">
+                 <img src={banner_red}></img>
+                 <div className="PointContainer">
+                    <div className="Points"><b>9.999</b></div>
+                    <div className="Nameline">AKTIV</div>
+                 </div>
+                 <div className="Star"><img style={{height: '16px'}} src={icon_star_white}></img></div>
             </div>
             <div className="Hygiene">
                  <img src={banner_blue}></img>
+                 <div className="PointContainer">
+                    <div className="Points"><b>9.999</b></div>
+                    <div className="Nameline">GESUNDHEIT</div>
+                 </div>
+                 <div className="Star"><img style={{height: '16px'}} src={icon_star_white}></img></div>
             </div>
-            <div className="Social">
-                 <img src={banner_red}></img>
+            <div className="Behavior">
+                 <img src={banner_orange}></img>
+                 <div className="PointContainer">
+                    <div className="Points"><b>9.999</b></div>
+                    <div className="Nameline">HELFEN</div>
+                 </div>
+                 <div className="Star"><img style={{height: '16px'}} src={icon_star_white}></img></div>
             </div>
         </div>
-
-    </div>
-    
+    </div>    
   );
 };
