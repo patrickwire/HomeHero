532692b3ab92c93c878d6440ed6d29b8ccd2426d
Patrick Müller <patrick@smokeless.world>
2020-03-22 13:12:44 +0100

    splash

--- src/screens/onboarding.tsx
+++ src/screens/onboarding.tsx
@@ -1,22 +1,63 @@
 import React, { useEffect, useState, useContext } from "react";
 import "./onboarding.css";
 import { intros } from "../data/intros";
-import { Fab } from "@material-ui/core";
+import { Fab, Button } from "@material-ui/core";
 import icon_arrow from "../assets/icon_arrow_purple.svg";
+import logo from "../assets/logo_vertical.svg";
 import { useHistory } from "react-router-dom";
 
 export const Onboarding = () => {
-  const [screen, setScreen] = useState(0);
-const history=useHistory()
+  const [screen, setScreen] = useState(-1);
+  const history = useHistory();
+  if (screen < 0) {
+    return (
+      <div className="onboardingScreen">
+        <div
+          style={{
+            height: 350,
+            flex: 1,
+            display: "inline-flex",
+            justifyContent: "center",
+            flexDirection: "column"
+          }}
+        >
+          <img src={logo} style={{width:"100%"}}/>
+      </div>
+      <div style={{ textAlign: "right", paddingTop: 30 }}>
+        <Fab
+          onClick={() => {
+            screen < 2 ? setScreen(screen + 1) : history.push("/register");
+          }}
+          aria-label="add"
+        >
+          <img src={icon_arrow} />
+        </Fab>
+      </div>
+      </div>
+    );
+  }
   const intro = intros[screen];
   return (
     <div className="onboardingScreen">
-      <div style={{height:350,flex:1,display:"inline-flex",justifyContent:"center",flexDirection:"column"}}>
+      <div
+        style={{
+          height: 350,
+          flex: 1,
+          display: "inline-flex",
+          justifyContent: "center",
+          flexDirection: "column"
+        }}
+      >
         <div className="title">{intro.title}</div>
         <div className="text">{intro.text || ""}</div>
       </div>
-      <div style={{ textAlign: "right", paddingTop: 30}}>
-        <Fab onClick={()=>{screen<2?setScreen(screen+1):history.push("/register")}} aria-label="add">
+      <div style={{ textAlign: "right", paddingTop: 30 }}>
+        <Fab
+          onClick={() => {
+            screen < 2 ? setScreen(screen + 1) : history.push("/register");
+          }}
+          aria-label="add"
+        >
           <img src={icon_arrow} />
         </Fab>
       </div>
