8f4e46266030bb4e7513d6f8bac40b0ffb4636f7
Patrick Müller <patrick@smokeless.world>
2020-03-22 13:47:42 +0100

    ANIMATION

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ActionCard.css";
 import icon_behavior from "../assets/icon_behavior.svg";
 import icon_social from "../assets/icon_social.svg";
@@ -12,7 +12,7 @@ import icon_arrow_red from "../assets/icon_arrow_red.svg";
 import icon_arrow_orange from "../assets/icon_arrow_orange.svg";
 import icon_arrow_blue from "../assets/icon_arrow_blue.svg";
 
-import { Button } from "@material-ui/core";
+import { Button, Fade } from "@material-ui/core";
 
 interface Props {
   type: any;
@@ -33,30 +33,33 @@ function pad(number: any, length: any) {
 }
 
 export const ActionCard = (props: Props) => {
-    const renderBackcard =(color:string,layer:number)=>{
-        return  (<div
+  const [fade, setFade] = useState(true);
+  const renderBackcard = (color: string, layer: number) => {
+    return (
+      <div
         className="shadow"
         style={{
           width: 380,
           height: 164,
           position: "absolute",
           background: color,
-          borderRadius: 32,zIndex:-1*layer,left:-8*layer,top:8*layer,
-        
-
+          borderRadius: 32,
+          zIndex: -1 * layer,
+          left: -8 * layer,
+          top: 8 * layer
         }}
-      ></div>)
-    }
+      ></div>
+    );
+  };
   if (props.type == "help") {
-    const backcards=[]
-    const bca=props.counter>4?4:props.counter
+    const backcards = [];
+    const bca = props.counter > 4 ? 4 : props.counter;
     for (let index = 0; index < bca; index++) {
-      backcards.push(renderBackcard("#E7394C",index+1))
-        
+      backcards.push(renderBackcard("#E7394C", index + 1));
     }
     return (
-      <div className="ActionCardHelp" style={{marginBottom:bca*8+15}}>
-          {backcards}
+      <div className="ActionCardHelp" style={{ marginBottom: bca * 8 + 15 }}>
+        {backcards}
         <span className="HeaderHelp">HELFEN</span>
         <span className="PointsHelp">{props.points}</span>
         <img
@@ -65,14 +68,23 @@ export const ActionCard = (props: Props) => {
           src={icon_star_red}
         ></img>
         <span className="DescriptionHelp">{props.title}</span>
-        <span className="CounterHelp">{pad(props.counter, 4)}</span>
+        <Fade in={fade}>
+          <span className="CounterHelp">{pad(props.counter, 4)}</span>
+        </Fade>
         <img className="ActionCardIcon" src={icon_social}></img>
         <div
           className="ButtonHelp"
           style={{ zIndex: 1000, display: props.onChange ? "block" : "none" }}
           onClick={() => {
-            // @ts-ignore
-            props.onChange("1");
+            setFade(false);
+
+            setTimeout(() => {
+              // @ts-ignore
+              props.onChange("1");
+            }, 500);
+            setTimeout(() => {
+              setFade(true);
+            }, 1000);
           }}
         >
           <span style={{ marginLeft: "16px" }}>HEUTE ERLEDIGT</span>
@@ -81,15 +93,14 @@ export const ActionCard = (props: Props) => {
       </div>
     );
   } else if (props.type == "active") {
-      const backcards=[]
-      const bca=props.counter>4?4:props.counter
-      for (let index = 0; index < bca; index++) {
-        backcards.push(renderBackcard("#FDB752",index+1))
-          
-      }
+    const backcards = [];
+    const bca = props.counter > 4 ? 4 : props.counter;
+    for (let index = 0; index < bca; index++) {
+      backcards.push(renderBackcard("#FDB752", index + 1));
+    }
     return (
-      <div className="ActionCardActive" style={{marginBottom:bca*8+15}}>
-          {backcards}
+      <div className="ActionCardActive" style={{ marginBottom: bca * 8 + 15 }}>
+        {backcards}
         <span className="HeaderActive">AKTIV BLEIBEN</span>
         <span className="PointsActive">{props.points}</span>
         <img
@@ -98,14 +109,23 @@ export const ActionCard = (props: Props) => {
           src={icon_star_orange}
         ></img>
         <span className="DescriptionHelp">{props.title}</span>
-        <span className="CounterActive">{pad(props.counter, 4)}</span>
+        <Fade in={fade}>
+          <span className="CounterHelp">{pad(props.counter, 4)}</span>
+        </Fade>
         <img className="ActionCardIcon" src={icon_behavior}></img>
         <div
           className="ButtonActive"
           style={{ zIndex: 1000, display: props.onChange ? "block" : "none" }}
           onClick={() => {
-            // @ts-ignore
-            props.onChange("1");
+            setFade(false);
+
+            setTimeout(() => {
+              // @ts-ignore
+              props.onChange("1");
+            }, 500);
+            setTimeout(() => {
+              setFade(true);
+            }, 1000);
           }}
         >
           <span style={{ marginLeft: "16px" }}>HEUTE ERLEDIGT</span>
@@ -114,15 +134,14 @@ export const ActionCard = (props: Props) => {
       </div>
     );
   } else {
-    const backcards=[]
-    const bca=props.counter>4?4:props.counter
+    const backcards = [];
+    const bca = props.counter > 4 ? 4 : props.counter;
     for (let index = 0; index < bca; index++) {
-      backcards.push(renderBackcard("#41DAE8",index+1))
-        
+      backcards.push(renderBackcard("#41DAE8", index + 1));
     }
     return (
-      <div className="ActionCardHealth" style={{marginBottom:bca*8+15}}>
-           {backcards}  
+      <div className="ActionCardHealth" style={{ marginBottom: bca * 8 + 15 }}>
+        {backcards}
         <span className="HeaderHealth">GESUNDHEIT</span>
         <span className="PointsHealth">{props.points}</span>
         <img
@@ -131,14 +150,23 @@ export const ActionCard = (props: Props) => {
           src={icon_star_blue}
         ></img>
         <span className="DescriptionHealth">{props.title}</span>
-        <span className="CounterHealth">{pad(props.counter, 4)}</span>
+        <Fade in={fade}>
+          <span className="CounterHealth">{pad(props.counter, 4)}</span>
+        </Fade>
         <img className="ActionCardIcon" src={icon_hygiene}></img>
         <div
           className="ButtonHealth"
           style={{ zIndex: 1000, display: props.onChange ? "block" : "none" }}
           onClick={() => {
-            // @ts-ignore
-            props.onChange("1");
+            setFade(false);
+
+            setTimeout(() => {
+              // @ts-ignore
+              props.onChange("1");
+            }, 500);
+            setTimeout(() => {
+              setFade(true);
+            }, 1000);
           }}
         >
           <span style={{ marginLeft: "16px" }}>HEUTE ERLEDIGT</span>
